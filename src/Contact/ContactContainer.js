99fbe923bb9a2b226aebef371fc74fb9451213ae
Josif Raburn <raburnjosif@gmail.com>
2018-03-18 13:06:55 -0600

    i added some of the social icons

--- src/Contact/ContactContainer.js
+++ src/Contact/ContactContainer.js
@@ -6,10 +6,18 @@ class ContactContainer extends Component {
         return(
             <div className = 'contact-container-main'>
                 <div className = 'contact-container'>
-                <video autoPlay muted loop id='myVideo' className = 'contact-video' >
-                    <source src='DemoReel2k17.mp4' />
-                </video>
-                    <div className = 'contact-box' >
+                    <video autoPlay muted loop id='myVideo' className = 'contact-video' >
+                        <source src='DemoReel2k17.mp4' />
+                    </video>
+                        {/* <div className = 'contact-box' >
+                        </div> */}
+                        <h1 className = 'contact-title' >
+                            contact.
+                        </h1>
+                    <div className = 'contact-social'>
+                        <img src = 'facebookicon.svg' className = 'facebook-icon' />
+                        <img src = 'twittericon.svg' className = 'facebook-icon' />
+                        <img src = 'youtubeicon.svg' className = 'facebook-icon' />
                     </div>
                 </div>
             </div>
