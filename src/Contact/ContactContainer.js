333bee5b34d825d3f713863e55a66f6dc9e5bd1c
Josif Raburn <raburnjosif@gmail.com>
2018-03-19 20:09:44 -0600

    I added the links in the social icons

--- src/Contact/ContactContainer.js
+++ src/Contact/ContactContainer.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './styles/ContactContainer.css'
 
 class ContactContainer extends Component {
     render() {
         return(
             <div className = 'contact-container-main'>
+                <Link to = '/' className = 'contact-go-back'>
+                    GO BACK
+                </Link>
                 <div className = 'contact-container'>
+
                     <video autoPlay muted loop id='myVideo' className = 'contact-video' >
                         <source src='DemoReel2k17.mp4' />
                     </video>
@@ -15,9 +20,15 @@ class ContactContainer extends Component {
                             contact.
                         </h1>
                     <div className = 'contact-social'>
-                        <img src = 'facebookicon.svg' className = 'facebook-icon' />
-                        <img src = 'twittericon.svg' className = 'facebook-icon' />
-                        <img src = 'youtubeicon.svg' className = 'facebook-icon' />
+                        <a href=''>
+                            <img src = 'facebookicon.svg' className = 'facebook-icon' />
+                        </a>
+                        <a href=''>
+                            <img src = 'twittericon.svg' className = 'facebook-icon' />
+                        </a>
+                        <a href=''>
+                            <img src = 'youtubeicon.svg' className = 'facebook-icon' />
+                        </a>
                     </div>
                 </div>
             </div>
